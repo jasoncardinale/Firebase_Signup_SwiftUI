f7058d51861e8622a91e4f9007e8d2c4aba7119c
Jason <jasoncardinale19@gmail.com>
2020-01-08 22:30:09 -0500

    Initial Commit

---
+++ Firebase_Signup_SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Firebase_Signup_SwiftUI
+//
+//  Created by Jason Cardinale on 1/8/20.
+//  Copyright © 2020 Jason Cardinale. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
